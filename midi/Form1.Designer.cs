d935e9a6e3817dd06299e3bdacaf24aa85872a97
Juan <juan.carosella@gmail.com>
2020-02-18 20:05:41 -0300

    sigue rota la conexión :c

--- midi/Form1.Designer.cs
+++ midi/Form1.Designer.cs
@@ -30,6 +30,7 @@
         {
             this.notaSalida = new System.Windows.Forms.Label();
             this.panel1 = new System.Windows.Forms.Panel();
+            this.button1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // notaSalida
@@ -41,7 +42,6 @@
             this.notaSalida.TabIndex = 0;
             this.notaSalida.Text = "label1";
             this.notaSalida.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
-            
             // 
             // panel1
             // 
@@ -49,13 +49,23 @@
             this.panel1.Name = "panel1";
             this.panel1.Size = new System.Drawing.Size(1181, 312);
             this.panel1.TabIndex = 1;
-            
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(1072, 22);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1223, 446);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.panel1);
             this.Controls.Add(this.notaSalida);
             this.Name = "Form1";
@@ -68,6 +78,7 @@
 
         private System.Windows.Forms.Label notaSalida;
         private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button button1;
     }
 }
 
--- midi/Form1.cs
+++ midi/Form1.cs
@@ -1,28 +1,29 @@
-﻿using Melanchall.DryWetMidi;
-using Melanchall.DryWetMidi.Common;
+﻿using Melanchall.DryWetMidi.Common;
 using Melanchall.DryWetMidi.Devices;
 
 using Melanchall.DryWetMidi.MusicTheory;
 using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
 using System.Drawing;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
+
+
 
 namespace midi
 {
     public partial class Form1 : Form
     {
 
-        
+            
+
+
+
+
+
         public Form1()
         {
 
-            string input = "F#";
+            
             InitializeComponent();
             TextBox.CheckForIllegalCrossThreadCalls = false;
 
@@ -39,49 +40,49 @@ namespace midi
             string[] blancasReturn = teclado.CrearTeclasBlancas(blancas, panel1);
             string[] notasTotal = negrasReturn.Concat(blancasReturn).ToArray();
 
-            foreach (var nota in notasTotal)
-            {
+            
 
-
-                foreach (Control p in panel1.Controls)
-                    if (p.Name == input)
-                    {
-                        p.BackColor = Color.AliceBlue;
-
-                    }
-                
-            }
 
            
-            var inputDevice = InputDevice.GetById(0);
+
+        var inputDevice = InputDevice.GetByName("CASIO USB-MIDI");
+        inputDevice.EventReceived += OnEventReceived;
+        inputDevice.StartEventsListening();
+
             
-                inputDevice.EventReceived += OnEventReceived;
-                inputDevice.StartEventsListening();
+            
+            
 
             var note1 = Note.Get((SevenBitNumber)100);
 
-
-
             
+            
+
 
             void OnEventReceived(object sender, MidiEventReceivedEventArgs e)
             {
 
+                inputDevice.Reset();
+                inputDevice.StartEventsListening();
+
+
                 foreach (Button p in panel1.Controls)
 
                 {
+
                     foreach (string thing in Negras)
                     {
                         if (p.Name == thing)
                         {
 
                             p.BackColor = Color.Black;
+                            
                         }
                     }
 
                     foreach (string thing in blancas)
-                        {
-                     if (p.Name == thing)
+                    {
+                        if (p.Name == thing)
                         {
 
                             p.BackColor = Color.White;
@@ -89,13 +90,18 @@ namespace midi
                     }
                 }
 
-                var midiDevice = (MidiDevice)sender;
                 
+                    
+
+                
+                var midiDevice = (MidiDevice)sender;
+
                 string entrada = e.Event.ToString();
                 string notaFinal = entrada.Split('(', ',')[1];
-             
+
                 string salida = (Note.Get((SevenBitNumber)Int32.Parse(notaFinal)).ToString());
                 salida = salida.Remove(salida.Length - 1);
+
                 notaSalida.Text = salida;
                 foreach (Button p in panel1.Controls)
                     if (p.Name == salida)
@@ -104,16 +110,21 @@ namespace midi
 
                     }
 
-
+                
             }
 
 
         }
 
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            
+
+
+        }
+
         
-
-
-
     }
 
 }
