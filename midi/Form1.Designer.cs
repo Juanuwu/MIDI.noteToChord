f77dd43983f52925546a2ee62b6155d9eb9deeb3
Juan <juan.carosella@gmail.com>
2020-06-05 06:59:33 -0300

    interfaz

--- midi/Form1.Designer.cs
+++ midi/Form1.Designer.cs
@@ -24,18 +24,16 @@
             this.notaSalida = new System.Windows.Forms.Label();
             this.panel1 = new System.Windows.Forms.Panel();
             this.label1 = new System.Windows.Forms.Label();
-            this.button1 = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // notaSalida
             // 
             this.notaSalida.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.notaSalida.Location = new System.Drawing.Point(163, 47);
+            this.notaSalida.Location = new System.Drawing.Point(271, 59);
             this.notaSalida.Name = "notaSalida";
             this.notaSalida.Size = new System.Drawing.Size(852, 60);
             this.notaSalida.TabIndex = 0;
-            this.notaSalida.Text = "label1";
+            this.notaSalida.Text = "Holi";
             this.notaSalida.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
             // panel1
@@ -52,37 +50,14 @@
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(304, 60);
             this.label1.TabIndex = 3;
-            this.label1.Text = "label1";
             this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
-            // 
-            // button1
-            // 
-            this.button1.Location = new System.Drawing.Point(1072, 22);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 2;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.GetRequestChord);
-            // 
-            // button2
-            // 
-            this.button2.Location = new System.Drawing.Point(1167, 22);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(75, 23);
-            this.button2.TabIndex = 4;
-            this.button2.Text = "button2";
-            this.button2.UseVisualStyleBackColor = true;
-            
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1425, 446);
-            this.Controls.Add(this.button2);
             this.Controls.Add(this.label1);
-            this.Controls.Add(this.button1);
             this.Controls.Add(this.panel1);
             this.Controls.Add(this.notaSalida);
             this.Name = "Form1";
@@ -95,9 +70,7 @@
 
         private System.Windows.Forms.Label notaSalida;
         private System.Windows.Forms.Panel panel1;
-        private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.Button button2;
     }
 }
 
--- midi/Form1.cs
+++ midi/Form1.cs
@@ -198,6 +198,7 @@ namespace midi
 ;            }
         }
 
+        
     }
 }
 
